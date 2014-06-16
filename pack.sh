4ae439e27bf1f35cd2dc0d04769d25fedb2226cc
Valerii Hiora <valerii.hiora@gmail.com>
2014-06-16 09:32:38 +0300

    Bumped version
    - Added CSS patch to remove left sidebar (based on @farcaller patch to dash-rust)
    - Bumped version
    - Ensuring that all existing docs are deleted before

--- pack.sh
+++ pack.sh
@@ -8,6 +8,8 @@ DOCSET_DIR="$OUT_DIR/$DOCSET_NAME"
 CONTENT_DIR="$DOCSET_DIR/Contents"
 DOC_DIR="$DOCSET_DIR/Contents/Resources/Documents"
 IDX_DIR="$DOCSET_DIR/Contents/Resources"
+
+rm -rf $DOC_DIR
 mkdir -p "$DOC_DIR"
 echo "Copying"
 cp -R "$SOURCE_DIR/" "$DOC_DIR"
@@ -16,17 +18,23 @@ cp icon.png $OUT_DIR/$DOCSET_NAME/
 echo "Indexing"
 find $DOC_DIR -type f -name \*.html | ./indexer.py $DOC_DIR $IDX_DIR
 cp RustNightly.xml $OUT_DIR
-cd $OUT_DIR
+
+echo "Patching CSS"
+cat >> $DOC_DIR/main.css <<EOF
+/* Dash DocSet overrides */
+
+.sidebar, .sub { display: none; }
+.content { margin-left: 0; }
+EOF
+
 echo "Packing"
+cd $OUT_DIR
 tar --exclude='.DS_Store' -czf $DOCSET_TGZ $DOCSET_NAME
 echo "Metadata"
 HASH=`openssl sha1 $DOCSET_TGZ | cut -d " " -f 2`
 DT=`date +%Y-%m-%d`
 cat ../RustNightly.xml | sed "s/__DT__/$DT/g;s/__SHA__/$HASH/g" > RustNightly.xml
 
-# upload to S3 xml + tgz
-# check permissions
 # dash-feed://https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fnet.vhbit.rust-doc%2FRustNightly.xml
 
 # pages with refresh
-
