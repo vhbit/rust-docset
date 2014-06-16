42c3c87a934eccc16c07f518a78f67ee4b6d419e
Valerii Hiora <valerii.hiora@gmail.com>
2014-06-16 19:07:52 +0300

    Fixed compat issues

--- pack.sh
+++ pack.sh
@@ -5,11 +5,11 @@ OUT_DIR="$2"
 DOCSET_NAME="RustNightly.docset"
 DOCSET_TGZ="RustNightly.tgz"
 
-rm -rf $DOC_DIR
-mkdir -p "$DOC_DIR"
+rm -rf $OUT_DIR
+mkdir -p $OUT_DIR
 
 echo "Creating docset"
-./indexer.py $DOC_DIR $OUT_DIR
+./indexer.py $SOURCE_DIR $OUT_DIR
 
 echo "Copying feed XML"
 cp RustNightly.xml $OUT_DIR
@@ -20,7 +20,7 @@ tar --exclude='.DS_Store' -czf $DOCSET_TGZ $DOCSET_NAME
 
 echo "Metadata"
 HASH=`openssl sha1 $DOCSET_TGZ | cut -d " " -f 2`
-DT=`date +%Y-%m-%d`
+DT=`date +%Y-%m-%d-%H-%M`
 cat ../RustNightly.xml | sed "s/__DT__/$DT/g;s/__SHA__/$HASH/g" > RustNightly.xml
 
 # dash-feed://https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fnet.vhbit.rust-doc%2FRustNightly.xml
