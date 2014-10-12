0162e0a446fc2884d4990ab228f95410d9826fc5
Valerii Hiora <valerii.hiora@gmail.com>
2014-10-12 14:58:59 +0300

    Nightly: add platforms support

--- tasks.py
+++ tasks.py
@@ -15,9 +15,17 @@ import tarfile
 from tempfile import TemporaryFile, mkdtemp
 import toml
 
-NIGHTLY_URL = "http://static.rust-lang.org/dist/rust-nightly-x86_64-unknown-linux-gnu.tar.gz"
-TAG_FILE = ".last-tag"
-DOC_PREFIX = "rust-nightly-x86_64-unknown-linux-gnu/doc"
+
+def nightly_url(platform):
+    return "http://static.rust-lang.org/dist/rust-nightly-%s.tar.gz" % platform
+
+
+def doc_prefix(platform):
+    return "rust-nightly-%s/doc" % platform
+
+
+def tag_file_name(platform):
+    return ".last-tag-%s" % platform
 
 
 def tag_from_response(req):
@@ -25,24 +33,24 @@ def tag_from_response(req):
     return req.headers['etag']
 
 
-def has_nightly_update():
+def has_update(url, tag_file):
     """Checks if tag was changed"""
-    if not os.path.exists(TAG_FILE):
+    if not os.path.exists(tag_file):
         return True
     else:
-        with open(TAG_FILE, "rt") as f:
+        with open(tag_file, "rt") as f:
             last_tag = f.read().strip()
 
-    r = requests.head(NIGHTLY_URL)
+    r = requests.head(url)
     new_tag = tag_from_response(r)
     return new_tag != last_tag
 
 
-def extract_docs(tar, dest_dir):
+def extract_docs(tar, dest_dir, doc_prefix):
     """Extracts only documentation files from nightly archive"""
     for info in tar.getmembers():
-        if info.name.startswith(DOC_PREFIX):
-            rel_path = os.path.relpath(info.name, DOC_PREFIX)
+        if info.name.startswith(doc_prefix):
+            rel_path = os.path.relpath(info.name, doc_prefix)
             dest_path = os.path.join(dest_dir, rel_path)
             if info.isdir():
                 if not os.path.exists(dest_path):
@@ -54,14 +62,17 @@ def extract_docs(tar, dest_dir):
 
 
 @task
-def update_nightly(force=False, out_dir="nightly_out"):
+def update_nightly(force=False, out_dir="nightly_out", platform = "x86_64-unknown-linux-gnu"):
     """Checks for update and re-builds doc if required. Stores new tag"""
+    url = nightly_url(platform)
+    tag_file = tag_file_name(platform)
+
     if not force:
-        if not has_nightly_update():
+        if not has_update(url, tag_file):
             print("No updates available yet")
             return
 
-    r = requests.get(NIGHTLY_URL, stream=True)
+    r = requests.get(url, stream=True)
     with TemporaryFile("w+b") as temp_file:
         print("Downloading")
         for chunk in r.iter_content(1024*1024*10):
@@ -73,12 +84,12 @@ def update_nightly(force=False, out_dir="nightly_out"):
             temp_file.seek(0)
             with tarfile.open(fileobj=temp_file, mode='r:gz',
                               bufsize=1024*1024*10) as tar:
-                extract_docs(tar, temp_dir)
+                extract_docs(tar, temp_dir, doc_prefix(platform))
 
             print("Building docset")
             build(doc_dir=temp_dir, out_dir=out_dir, conf="nightly.toml")
 
-            with open(TAG_FILE, "w+t") as f:
+            with open(tag_file, "w+t") as f:
                 f.write(tag_from_response(r))
         except:
             import traceback
